1c036b87c0d1881c239ac92723c400454c34f33e
jmanzanares12 <jimgken2004@gmail.com>
2025-03-29 00:29:38 -0600

    Drop: API Key

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
-import { lazy } from "react"
+import { lazy, Suspense } from "react"
 import { Routes, Route } from "react-router-dom"
 
 const Home = lazy(() => import("./views/Home"))
 const Details = lazy(() => import("./views/Details"))
 
 function App() {
+
   return (
     <div className="overflow-y-auto max-h-full p-12">
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/:zodiac" element={<Details />} />
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/:zodiac" element={<Details />} />
+        </Routes>
+      </Suspense>
     </div>
   )
 }
--- src/components/ZodiacDetails.tsx
+++ src/components/ZodiacDetails.tsx
@@ -2,21 +2,24 @@ import { Link } from "react-router-dom";
 import { useMemo } from "react";
 import { useParams } from "react-router-dom";
 import { getZodiacImage } from "../utils/getZodiacImage";
-import { useLocalZodiacDetails } from "../hooks/useLocalZodiacDetails";
 import { Zodiac } from "../types/zodiac";
+import { useLocalZodiacDetails } from "../hooks/useLocalZodiacDetails";
 
 const ZodiacDetails = () => {
-    const {zodiac} = useParams();
-
+    const { zodiac } = useParams();
     const imgSrc = useMemo(() => getZodiacImage(zodiac as Zodiac), [zodiac]);
-    const data = useLocalZodiacDetails(zodiac as Zodiac) || 'No data found';
+    const data = useLocalZodiacDetails(zodiac as Zodiac) || 'No data found'; // Usamos la función de hook local para obtener los datos
 
     return (
         <div className="w-3/4 p-12 rounded-lg border-1 border-gray-700 flex flex-col 
             justify-center items-center backdrop-blur-xl shadow-xl m-auto ">
             <img src={imgSrc} alt={zodiac} className="w-36 h-36 rounded-full mb-4" />
             <h1 className="text-4xl font-bold text-white mt-2 mb-2">{zodiac}</h1>
+
             <p className="text-center text-white font-semibold mt-2 mb-2">{data}</p>
+
+            {data && <p className="text-center text-white font-semibold mt-2 mb-2">{data}</p>}
+
             <Link to='/'><button className="bg-white text-black rounded-lg px-4 py-2 mt-4">Back</button></Link>
         </div>
     )
