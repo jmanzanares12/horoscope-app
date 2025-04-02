aea6498eaa362fda9fe159141f3f4f8da4bf550f
jmanzanares12 <jimgken2004@gmail.com>
2025-04-01 22:31:15 -0600

    Reset: commits

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { lazy, Suspense } from "react"
+import { lazy } from "react"
 import { Routes, Route } from "react-router-dom"
 
 const Home = lazy(() => import("./views/Home"))
@@ -8,12 +8,10 @@ function App() {
 
   return (
     <div className="overflow-y-auto max-h-full p-12">
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/:zodiac" element={<Details />} />
-        </Routes>
-      </Suspense>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:zodiac" element={<Details />} />
+      </Routes>
     </div>
   )
 }
